c019cd44162f67db88c877bf8c74704d21bf49f0
usera2tt <usera2tt@gmail.com>
2022-02-28 20:06:41 +0900

    chore: increase save debounce

--- src/js/constants.js
+++ src/js/constants.js
@@ -1,4 +1,4 @@
 export const KEY_COLOR = "KEY_COLOR"
 
-export const SAVE_DEBOUNCE = 500  // in milli seconds
+export const SAVE_DEBOUNCE = 1000  // in milli seconds
 export const CSS_CLASS = 'darkmode-png';
